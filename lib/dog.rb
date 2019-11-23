42efdd8d778b5d1754b40d67fe90117074efc511
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-23 22:55:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,2 @@
 class Dog 
-  def self.define_properties(props)
-    props.each do |prop|
-      define_method(prop) do
-        instance_variable_get(:"@#{prop}")
-    end
-  end
-end
-  self.define_properties([:name, :breed])
-end
+  
